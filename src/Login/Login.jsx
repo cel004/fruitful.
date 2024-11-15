11b962be6b924886c936023a5c8dd1d4bf4a51cb
cel004 <ngcelinaa@gmail.com>
2024-11-15 02:16:30 -0500

    Create mock sign in interface

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,18 +1,45 @@
 import styles from './Login.module.css'
+import { useState } from 'react';
 
-function Login(){
-    return(
+function Login() {
+    const [email, setEmail] = useState('Enter your email');
+    const [password, setPassword] = useState('Enter your password');
+
+    const submitButton = (e) => {
+        e.preventDefault();
+        // Handle form submission here
+        console.log('Form submitted:', { name, email });
+    };
+
+    return (
         <header>
-            <div className={styles.sodaImage}>
-                <img src={melonSodaImage} alt="Pixel art of japanese melon cream soda"/> 
-            </div>
-            <div className={styles.title}>
+            <div className={styles.titleContainer}>
                 <p>Save-a-Sip</p>
-                <p className={styles.slogan}>Small savings, sweet rewards<p/>
             </div>
-            <p>font constant</p>
+            <p className ={styles.slogan}>Small savings, sweet rewards</p>
+            <div className={styles.formContainer}>
+                <form onSubmit={submitButton}>
+                    <label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    <button type="submit">Log in</button>
+                </form>
+            </div>
         </header>
-    )
+    );
 }
 
 export default Login
