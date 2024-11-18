c4c64f21d0f69f508c0fa2c84314e3296ed3469e
cel004 <ngcelinaa@gmail.com>
2024-11-18 00:00:54 -0500

    Create placeholder piechart for layout

--- src/components/Piechart/Piechart.jsx
+++ src/components/Piechart/Piechart.jsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import styles from './Piechart.module.css';
 
 
 function Piechart(){
     return(
-        <div className={styles.pieChartContainer}>
-            <div className={styles.pieChart}></div>
+        <div className={styles.piechartContainer}>
+            <div className={styles.piechart}></div>
         </div>
     )
 }
