94a157e2017d61308d8e792529a914af6cc44e37
cel004 <ngcelinaa@gmail.com>
2024-11-19 02:20:29 -0500

    Create container components

--- src/pages/HomeScreen/HomeScreen.jsx
+++ src/pages/HomeScreen/HomeScreen.jsx
@@ -3,20 +3,22 @@ import WelcomeHeader from '../../components/WelcomeHeader/WelcomeHeader';
 import Piechart from '../../components/Piechart/Piechart';
 import Goal from '../../components/Goal/Goal';
 import Navigation from '../../components/Navigation/Navigation';
+import PageWrapper from '../../components/PageWrapper/PageWrapper';
+import MainContent from '../../components/MainContent/MainContent';
 
 
 function HomeScreen(){
     document.body.style.backgroundImage = "none";
     document.body.style.backgroundImage = "linear-gradient(#d82d66b6 10%, #d82d663a 50%, #ffffff 80%)";
     return (
-        <div className={styles.pageWrapper}>
-            <main className={styles.mainContent}>
+        <PageWrapper>
+            <MainContent>
                 <WelcomeHeader />
                 <Piechart />
                 <Goal />
-            </main>
+            </MainContent>
                 <Navigation />
-        </div>
+        </PageWrapper>
         
     );
 }
