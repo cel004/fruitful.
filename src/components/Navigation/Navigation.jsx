3a870f21ae4032586d874b432955f129db221966
cel004 <ngcelinaa@gmail.com>
2024-11-19 01:45:21 -0500

    Merge create button with navigation bar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,15 +2,21 @@ import styles from './Navigation.module.css';
 import { Card } from '../icons/iconCard.jsx';
 import { Home } from '../icons/iconHome.jsx';
 import { Piechart } from '../icons/iconPiechart.jsx';
+import Create from '../Create/Create'
 
 import { Link } from 'react-router-dom';
 
 function Navigation() {
     return (
-        <div className={styles.navigationContainer}>
-            <div className={styles.navigationIcon}><Link to="/transactions"><Card /></Link></div>
-            <div className={styles.navigationIcon}><Link to="/homescreen"><Home /></Link></div>
-            <div className={styles.navigationIcon}><Link to="/budget"><Piechart /></Link></div>
+        <div className={styles.footer}>
+            <div className={styles.createButton}>
+                    <Create />
+                </div>
+            <div className={styles.navigationContainer}>
+                <div className={styles.navigationIcon}><Link to="/transactions"><Card /></Link></div>
+                <div className={styles.navigationIcon}><Link to="/homescreen"><Home /></Link></div>
+                <div className={styles.navigationIcon}><Link to="/budget"><Piechart /></Link></div>
+            </div>
         </div>
     );
 }
